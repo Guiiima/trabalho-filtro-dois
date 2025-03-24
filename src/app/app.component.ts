6b2746da613a8137854affb0a19ea2af0197c1d5
Seu Nome <guilhermeh000@hotmail.com>
2025-03-24 20:55:58 -0300

    trabalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trabalho';
+  imageSrc: string | ArrayBuffer | null = null;
+  imageFilter: string = '';
+  imageRotation: string = '';
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageSrc = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('Por favor, selecione apenas um arquivo de imagem.');
+    }
+  }
+   applyBlur(): void {
+    this.imageFilter = 'blur(5px)';
+  }
+
+  applySharpening(): void {
+    this.resetFilters()
+    this.imageFilter = 'url(#sharpeningFilter)';
+   // this.imageFilter = 'contrast(200%) brightness(120%)';
+  }
+
+  rotateImage(): void {
+    this.resetFilters()
+    this.imageRotation = 'rotate(45deg)';
+  }
+  private resetFilters(): void {
+    this.imageFilter = '';
+    this.imageRotation = '';
+  }
 }
